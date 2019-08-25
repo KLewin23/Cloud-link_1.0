5758a9117a502dfd01cb49f65620813ae4d27c55
Kieran Lewin <kieran.lewin-cic-uk@ibm.com>
2019-08-26 00:46:54 +0100

    Build Config Working

--- public/electron-starter.js
+++ public/electron-starter.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -18,7 +19,7 @@ function createWindow () {
 
     // and load the index.html of the app.
     const startUrl = process.env.ELECTRON_START_URL || url.format({
-        pathname: path.join(__dirname, '/../build/index.html'),
+        pathname: path.join(__dirname, '../build/index.html'),
         protocol: 'file:',
         slashes: true
     });
